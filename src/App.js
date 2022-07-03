26412c25b24fe711edc53f1ce2dc3df510f03bb8
Daisuke Ogawa <40881014+d-ogawa@users.noreply.github.com>
2022-07-03 12:45:12 +0900

    fix

--- src/App.js
+++ src/App.js
@@ -1,35 +1,7 @@
 import "./styles.css";
-import { PrimaryButton } from "./components/atoms/button/PrimaryButton";
-import { SecondaryButton } from "./components/atoms/button/SecondaryButton";
-import { SearchInput } from "./components/molecules/SearchInput";
-import { UserCard } from "./components/organisms/user/UserCard";
-import { HeaderOnly } from "./components/templates/HeaderOnly";
-import { BrowserRouter } from "react-router-dom";
-import { DefaultLayout } from "./components/templates/DefaultLayout";
+
 import { Router } from "./router/Router";
 
-const user = {
-  name: "じゃけぇ",
-  image: "https://source.unsplash.com/N04FIfHhv_k",
-  email: "12345@example.com",
-  phone: "000-1111-2222",
-  company: {
-    name: "テスト株式会社"
-  },
-  website: "https://google.com"
-};
-
 export default function App() {
-  return (
-    <Router />
-    // <BrowserRouter>
-    //   <DefaultLayout>
-    //     <PrimaryButton>テスト</PrimaryButton>
-    //     <SecondaryButton>検索</SecondaryButton>
-    //     <br />
-    //     <SearchInput />
-    //     <UserCard user={user} />
-    //   </DefaultLayout>
-    // </BrowserRouter>
-  );
+  return <Router />;
 }
